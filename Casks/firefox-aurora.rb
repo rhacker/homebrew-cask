c42c55634e434ec6ff96d8a3a74e1d5d08d15618
rhacker <nxqd.inbox+rhacker@gmail.com>
2013-09-24 10:40:58 +0200

    update firefox aurora

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,7 +1,7 @@
 class FirefoxAurora < Cask
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-25.0a2.en-US.mac.dmg'
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-26.0a2.en-US.mac.dmg'
   homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
-  version '25.0a2'
-  sha1 'ea9a343ee322ae2e243df2bc0b23ce25005cb46f'
+  version '26.0a2'
+  sha1 '0d27707432c5146f605eed276df9aad31d64db35'
   link 'FirefoxAurora.app'
 end
